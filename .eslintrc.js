94084a494fa497c86fc2b74a68182e2dc4518484
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-10-15 18:47:15 +0200

    format all files with Prettier (updated)
    npx prettier . --write

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 export default {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended', // Integrate Prettier with ESLint
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    rules: {
-        'import/no-commonjs': 'off', 
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended", // Integrate Prettier with ESLint
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "import/no-commonjs": "off",
+  },
 };
