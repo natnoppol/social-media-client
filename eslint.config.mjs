9c87b681d66649443de79714f515eca753570787
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-10-15 20:45:49 +0200

    test husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,25 @@ import pluginJs from '@eslint/js';
 export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
+	{
+		env: {
+			browser: true,
+			es2021: true,
+			node: true
+		},
+		extends: [ 
+			'eslint:recommended', 
+			'prettier'
+		],
+		files: ['**/*.js'],
+		parserOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+		},
+		rules: {
+			'no-unused-vars': 'warn', 
+      		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+			'no-console':'error'
+		},
+	},
 ];
