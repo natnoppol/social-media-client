f67f7f6c55c371e9257cae816353f2060df547ad
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-10-19 23:57:26 +0200

    cypress tested

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,cy.js}",
-    baseUrl: "http://localhost:8080"
-  },
+	e2e: {
+		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,cy.js}',
+		baseUrl: 'http://localhost:8080',
+	},
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,41 +4,41 @@ import jest from 'eslint-plugin-jest';
 import cypress from 'eslint-plugin-cypress';
 
 export default [
-  {
-    files: ['src/**/*.js', 'cypress/**/*.js'], // Include your Cypress test files
-    ignores: [
-      'node_modules/*', // ignore its content
-    ],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        describe: 'readonly', // Jest and Mocha
-        it: 'readonly',       // Jest and Mocha
-        expect: 'readonly',   // Jest
-        cy: 'readonly',       // Cypress
-        beforeEach: 'readonly', // Jest
-        global: 'readonly',
-        jest: 'readonly', 
-      },
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'comma-dangle': ['error', 'always-multiline'],
-      'no-console': 'warn',
-      'no-unused-vars': 'warn',
-      indent: ['error', 2],
-      quotes: ['error', 'single'],
-    },
-  },
-  // Add plugin configurations directly here instead of using extends
-  pluginJs.configs.recommended,
-  {
-    plugins: {
-      jest,
-      cypress,
-    },
-  },
+	{
+		files: ['src/**/*.js', 'cypress/**/*.js'], // Include your Cypress test files
+		ignores: [
+			'node_modules/*', // ignore its content
+		],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				describe: 'readonly', // Jest and Mocha
+				it: 'readonly', // Jest and Mocha
+				expect: 'readonly', // Jest
+				cy: 'readonly', // Cypress
+				beforeEach: 'readonly', // Jest
+				global: 'readonly',
+				jest: 'readonly',
+			},
+			parserOptions: {
+				ecmaVersion: 2021,
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'comma-dangle': ['error', 'always-multiline'],
+			'no-console': 'warn',
+			'no-unused-vars': 'warn',
+			indent: ['error', 2],
+			quotes: ['error', 'single'],
+		},
+	},
+	// Add plugin configurations directly here instead of using extends
+	pluginJs.configs.recommended,
+	{
+		plugins: {
+			jest,
+			cypress,
+		},
+	},
 ];
